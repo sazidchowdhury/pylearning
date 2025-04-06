5bdf5cd760b2ac582194b34658cb5ebe82488e66
sazidchowdhury <azmsaz97@gmail.com>
2025-04-07 00:00:25 +0600

    Day 6 Ex-1

---
+++ pyhdoc/day_6/day_6_1.py
@@ -0,0 +1,13 @@
+## These script should be run on https://reeborg.cs20.ca/
+
+## Exercise - 1: Turn Around
+def turn_around():
+    turn_left()
+    turn_left()
+
+move()
+move()
+turn_around()
+move()
+move()
+turn_around()
