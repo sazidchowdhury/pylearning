3eea267fbb49c281d17badfa2330d137ff2a02fe
sazidchowdhury <azmsaz97@gmail.com>
2025-05-06 12:07:26 +0600

    Day_8 start-2

--- pyhdoc/day_8/day_8_start.py
+++ pyhdoc/day_8/day_8_start.py
@@ -1,6 +1,13 @@
-def greet():
-    print("Hello")
-    print("How do you do?")
-    print("Isn't the weather nice today?")
+# def greet():
+#     print("Hello")
+#     print("How do you do?")
+#     print("Isn't the weather nice today?")
     
-greet()
+# greet()
+
+def greet_with_input(name):
+    print(f"Hello {name}")
+    print(f"How do you do, {name}?")
+    print(f"Isn't the weather nice today, {name}?")
+    
+greet_with_input("Princess")
