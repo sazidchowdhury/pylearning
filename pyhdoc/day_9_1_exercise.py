77d78ba0afd5717395ab4e62e7adaa5ffb3331b3
sazidchowdhury <azmsaz97@gmail.com>
2025-05-07 14:05:20 +0600

    Day_9 Grading Program Exercise

---
+++ pyhdoc/day_9_1_exercise.py
@@ -0,0 +1,29 @@
+# Grading Program
+
+# 🚨 Don't change the code below 👇
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+student_grades = {}
+
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_grades[student] = "Outstanding"
+  elif score > 80:
+    student_grades[student] = "Exceeds Expectations"
+  elif score > 70:
+    student_grades[student] = "Acceptable"
+  else:
+    student_grades[student] = "Fail"
+
+print(student_grades)
+
