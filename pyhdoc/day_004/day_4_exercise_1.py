a93ea8b5ddb6855bb8eda4561692e5e4618a04fb
sazidchowdhury <azmsaz97@gmail.com>
2025-05-08 22:20:57 +0600

    Day-4 Coin Toss Exercise

---
+++ pyhdoc/day_004/day_4_exercise_1.py
@@ -0,0 +1,12 @@
+# Coin Toss
+
+import random
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
