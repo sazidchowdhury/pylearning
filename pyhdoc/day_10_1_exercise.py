bc30dcbfd94e862922ea766c05ad1be10459b118
sazidchowdhury <azmsaz97@gmail.com>
2025-05-08 19:42:19 +0600

    Day_10 DiM added check for valid month

--- pyhdoc/day_10_1_exercise.py
+++ pyhdoc/day_10_1_exercise.py
@@ -13,6 +13,8 @@ def is_leap(year):
         return False
 
 def days_in_month(input_year, input_month):
+    if input_month > 12 or input_month < 1:
+        return "Invalid month"
     input_month = input_month - 1
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if is_leap(year=input_year):
