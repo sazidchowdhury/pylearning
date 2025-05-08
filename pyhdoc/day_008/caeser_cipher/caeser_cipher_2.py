83c7ccbb4c3e5e61fbf75b282d0dca742e36adf1
sazidchowdhury <azmsaz97@gmail.com>
2025-05-08 23:09:27 +0600

    Day-8 Caeser Cipher Step-2

---
+++ pyhdoc/day_008/caeser_cipher/caeser_cipher_2.py
@@ -0,0 +1,95 @@
+# Caeser Cipher Step 2
+
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+
+while direction not in ["encode", "decode"]:
+    print("Choose a valid option.")
+    direction = input("Type 'encode' to encrypt or 'decode' to decrypt:\n").lower()
+
+# Once 'encode' or 'decode' is confirmed, proceed
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+
+    print(f"The encoded text is {cipher_text}")
+
+
+def decrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+
+    print(f"The encoded text is {cipher_text}")
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+
+elif direction == "decode":
+    decrypt(plain_text=text, shift_amount=shift)
