41bb3fdd8e37ce0ab24864e414ec7f21e16686fd
sazidchowdhury <azmsaz97@gmail.com>
2025-05-08 23:20:38 +0600

    Day-10 Days in Month Exercise

---
+++ pyhdoc/day_010/day_10_exercise_1.py
@@ -0,0 +1,38 @@
+# Days in month
+
+
+def is_leap(year):
+    """Return True for leap years, False for non-leap years."""
+
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def days_in_month(input_year, input_month):
+    """Return number of days in that month in that year."""
+
+    if input_year < 0:
+        return "Invalid year"
+    if input_month > 12 or input_month < 1:
+        return "Invalid month"
+    input_month = input_month - 1
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year=input_year):
+        month_days[1] = month_days[1] + 1  # Adjust February for leap year
+    month_day = month_days[input_month]
+    return month_day
+
+
+# 🚨 Do NOT change any of the code below
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
