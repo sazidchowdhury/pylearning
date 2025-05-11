7d05468e9d7c9cc7177a23535fc9b56e18d9b5fb
sazidchowdhury <azmsaz97@gmail.com>
2025-05-11 23:22:36 +0600

    Day-13 Debugging Odd Even

---
+++ pyhdoc/day_013/day_13_exercise_1.py
@@ -0,0 +1,6 @@
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:  # debugged wrong operator =
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
