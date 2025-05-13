eb16ded7f3fc568683d9ab6ab96419af1cafb560
sazidchowdhury <azmsaz97@gmail.com>
2025-05-14 00:48:11 +0600

    Day-15 Coffee Machine Project

---
+++ pyhdoc/day_016/day_16_start.py
@@ -0,0 +1,16 @@
+# import another module
+# print (another_module.another_variable)
+#
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("CadetBlue")
+timmy.forward(100)
+timmy.back(100)
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
