7a1fb58690f9234124ce5602c5eac6414d56b0ef
sazidchowdhury <azmsaz97@gmail.com>
2025-05-18 00:22:22 +0600

    Day-17 Quiz data changed

--- pyhdoc/day_017/quiz_game/data.py
+++ pyhdoc/day_017/quiz_game/data.py
@@ -1,14 +1,12 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {"text": "The shortest war in history was between Britain and Zanzibar on August 27, 1896, and lasted only 38 minutes.", "answer": "True"},
+    {"text": "The longest word in the English language has 45 letters.", "answer": "True"},
+    {"text": "The world's longest recorded flight of a chicken is 13 seconds.", "answer": "True"},
+    {"text": "The world's fastest recorded typist can type up to 256 words per minute.", "answer": "True"},
+    {"text": "The world's largest waterfall, by volume of water, is actually located underwater.", "answer": "True"},
+    {"text": "The world's longest recorded duration without sleep is 264.4 days.", "answer": "False"},
+    {"text": "The world's largest living organism is a fungus.", "answer": "True"},
+    {"text": "The world's largest snowflake was 15 inches in diameter.", "answer": "True"},
+    {"text": "The world's longest recorded duration of hiccups lasted 21 years.", "answer": "False"},
+    {"text": "The world's largest strawberry was over 1.5 kg.", "answer": "True"}
 ]
